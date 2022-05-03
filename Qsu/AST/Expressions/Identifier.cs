14d70f4a8ab15d823464e4f661cd95a9614cc9ac
KYapp <82925909+kukiyomenaisapp@users.noreply.github.com>
2022-05-03 16:01:22 +0900

    AST自体の定義をした
    パーサーつくりたくないいいいいいいいい

---
+++ Qsu/AST/Expressions/Identifier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Qsu.Lexing;
+
+namespace Qsu.AST.Expressions
+{
+    public class Identifier : IExpression
+    {
+        public Token Token;
+        public string Value;
+
+        public Identifier(Token token,string value)
+        {
+            Token = token;
+            Value = value;
+        }
+
+        public string TokenLiteral() => Token.Literal;
+    }
+}
---
+++ Qsu/AST/Statements/LetStatement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Qsu.Lexing;
+using Qsu.AST.Expressions;
+
+namespace Qsu.AST.Statements
+{
+    public class LetStatement : IStatement
+    {
+        public Token Token;
+        public Identifier Name;
+        public IExpression Value;
+
+        public string TokenLiteral() => Token.Literal;
+    }
+}
