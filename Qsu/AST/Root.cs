ec7e366db5c0e15a77584e9cff81163e004783dc
KYapp <82925909+kukiyomenaisapp@users.noreply.github.com>
2022-05-03 15:49:46 +0900

    ルートノード作った

---
+++ Qsu/AST/Root.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Qsu.AST
+{
+    public class Root : INode
+    {
+        public List<IStatement> Statements;
+
+        public string TokenLiteral()
+        {
+            return Statements.FirstOrDefault()?.TokenLiteral() ?? "";
+        }
+    }
+}
