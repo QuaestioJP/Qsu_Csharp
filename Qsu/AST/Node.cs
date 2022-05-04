5e9fb98b355b4c5365449b82f70b8a54e64dbbfa
KYapp <82925909+kukiyomenaisapp@users.noreply.github.com>
2022-05-04 20:00:41 +0900

    パーサーの側だけ作った

---
+++ Qsu/AST/Node.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Qsu.AST
+{
+    /// <summary>
+    /// 全てのノードが実装しているインターフェース
+    /// </summary>
+    public interface INode
+    {
+        
+    }
+
+    /// <summary>
+    /// 全ての文が実装しているインターフェース
+    /// </summary>
+    public interface IStatement : INode
+    {
+
+    }
+
+    /// <summary>
+    /// 全ての式が実装しているインターフェース
+    /// </summary>
+    public interface IExpression : INode
+    {
+
+    }
+}
---
+++ Qsu/Parser/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Qsu.Lexing;
+
+namespace Qsu.Parsing
+{
+    public class Parser
+    {
+        public Token CurrentToken;
+        public Token NextToken;
+        public Lexer Lexer;
+        
+        public Parser(Lexer lexer)
+        {
+            Lexer = lexer;
+
+            CurrentToken = Lexer.NextToken();
+            NextToken = Lexer.NextToken();
+        }
+
+        public void ReadToken()
+        {
+            CurrentToken = NextToken;
+            NextToken = Lexer.NextToken();
+        }
+    }
+}
